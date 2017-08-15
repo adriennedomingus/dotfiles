bab251a518400af800de3e6ee4d1001ac13761c6
Adrienne Domingus <adrienne.domingus@gmail.com>
2017-08-14 19:51:46 -0700

    Split into first second, new iterm profile for devo

---
+++ first.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo xcodebuild -license
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" # Homebrew
+brew update;
+brew doctor;
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"; # oh-my-zsh
--- second.sh
+++ second.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-sudo xcodebuild -license
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" # Homebrew
-brew update;
-brew doctor;
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"; # oh-my-zsh
 brew install wget;
+brew cask install iterm2;
 brew cask install dropbox;
 brew cask install 1password;
 brew cask install flux;
@@ -13,19 +9,11 @@ brew cask install atom;
 brew cask install pycharm;
 brew cask install google-chrome;
 brew cask install google-play-music-desktop-player;
-brew install libxml2;
-brew install libxslt;
-brew install imagemagick ghostscript;
-brew install libmemcached;
-brew install libxmlsec1;
-brew install homebrew/versions/swig304;
-brew install pkg-config libffi;
 brew install postgresql;
 brew install heroku;
+brew install gpg;
 sudo easy_install pip;
-pip install virtualenvwrapper;
-export WORKON_HOME=~/Envs;
-source /usr/local/bin/virtualenvwrapper.sh;
+sudo pip install virtualenvwrapper;
 ln -sn ~/personal_projects/dotfiles/gitconfig.symlink ~/.gitconfig;
 mv ~/.zshrc ~/.zshrc.bak;
 ln -sn ~/personal_projects/dotfiles/robbyrussell-adrienne.zsh-theme ~/.oh-my-zsh/themes/;
